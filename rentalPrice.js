32b71e74f73f63738f257ec3a9245f6201723b7e
Jüri Kajava <73351309+kajava-juri@users.noreply.github.com>
2023-02-27 13:16:22 +0200

    refactored

--- rentalPrice.js
+++ rentalPrice.js
@@ -1,23 +1,23 @@
 // age - age of driver
 // licence - number of full years person holds driving licence
-// clazz - class of the car from 1 (smallest) to 5 (largest) that person wishes to rent
-// acc - has s/he caused any accidents within last year
-// acc2 - has s/he participated (but not caused) in any accidents within last year
-// season - if it is high season or not
-function price(age, licence, clazz, acc, acc2, season) {
+// rentalClass - class of the car from 1 (smallest) to 5 (largest) that person wishes to rent
+// causedAccidents - has s/he caused any accidents within last year
+// participatedAccidents - has s/he participated (but not caused) in any accidents within last year
+// highSeason - if it is high season or not
+function price(age, licence, rentalClass, causedAccidents, participatedAccidents, highSeason) {
 
     if (age < 18)
     {
       return "Driver too young - cannot quote the price";
     }
-    if (age <= 21 && clazz > 2)
+    if (age <= 21 && rentalClass >= 2)
     {
         return "Drivers 21 y/o or less can only rent Class 1 vehicles";
     }
     var rentalprice = age;
-    if (clazz >= 4 && age <= 25 && season !== false)
+    if (rentalClass >= 4 && age <= 25 && highSeason === true)
     {
-      rentalprice = rentalprice * 2;
+      rentalprice *= 1.5;
     }
     if (licence < 1)
     {
@@ -25,11 +25,14 @@ function price(age, licence, clazz, acc, acc2, season) {
     }
     if (licence < 3)
     {
-      rentalprice = rentalprice * 1.3;
+      rentalprice *= 1.3;
     }
-    if (acc == true && age < 30)
+    if (causedAccidents == true && age < 30)
     {
-      rentalprice = rentalprice + 15;
+      rentalprice += 15;
+    }
+    if(rentalprice < age){
+      return age;
     }
     if (rentalprice > 1000)
     {
