d6779883b293ff535e7d52fe702b4fee0cc953b3
MaksimVerzbitski <maksim.verzbitski@gmail.com>
2023-02-28 14:56:14 +0200

    Separate calculaterentalPrice(post) and respondWithResult

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const http = require('http')
+/* const http = require('http')
 const qs = require('querystring')
 //const rental = require('./rentalPrice')
 const rental = require('./rentalPriceTesting')
@@ -84,4 +84,98 @@ const port = 3001
 const host = 'localhost'
 server.listen(port, host)
 console.log(`Listening at http://${host}:${port}`)
-//return;
+//return; */
+const http = require('http')
+const qs = require('querystring')
+const rental = require('./rentalPriceTesting')
+
+const server = http.createServer(function (request, response) {
+  console.dir(request.param)
+
+  if (request.method == 'POST') {
+    console.log('POST')
+    var body = ''
+    request.on('data', function (data) {
+      body += data
+    })
+
+    request.on('end', function () {
+      const post = qs.parse(body)
+      console.log(post);
+      const rentalPrice = calculateRentalPrice(post)
+      respondWithResult(response, post, rentalPrice)
+    })
+  } else {
+    const html = `
+      <html>
+        <body>
+          <form id="calcForm" method="post" action="http://localhost:3001">
+            Driver: <br>
+            <input type="number" name="age"/>
+            <label for="html">Your age?</label><br>
+            <input type="number" name="licence" />
+            <label for="html">How long have you had your licence?</label><br>
+            <input type="number" name="clazz" />
+            <label for="html">Class of the car (1 to 5)</label><br>
+            <input type=checkbox name="hasCausedAccidents" />
+            <label for="html">Have you caused accidents?</label><br>
+            <input type=checkbox name="hasParticipatedInAccidents" />
+            <label for="html">Have you participated in any accidents?</label><br>
+            <input type=checkbox name="isHighSeason" />
+            <label for="html">Is it high season?</label><br>
+            <input type="submit" value="Add" />
+          </form>
+        </body>
+      </html>`
+    response.writeHead(200, { 'Content-Type': 'text/html' })
+    response.end(html)
+  }
+
+  function parseBool(val) {
+    return val === 'on'
+  }
+
+  function calculateRentalPrice(post) {
+    return rental.calculateRentalPrice(
+      Number(post.age),
+      Number(post.licence),
+      Number(post.clazz),
+      parseBool(post.hasCausedAccidents),
+      parseBool(post.hasParticipatedInAccidents),
+      parseBool(post.isHighSeason)
+    )
+  }
+
+  function respondWithResult(response, post, rentalPrice) {
+    response.writeHead(200, { 'Content-Type': 'text/html' })
+    if (rentalPrice.success) {
+      response.end(`
+        <html>
+          <body>
+            <h1>Rental price calculation result:</h1>
+            <p>Age: ${post.age}</p>
+            <p>Licence duration: ${post.licence}</p>
+            <p>Class of the car: ${post.clazz}</p>
+            <p>Have you caused accidents? ${post.hasCausedAccidents ? 'Yes' : 'No'}</p>
+            <p>Have you participated in accidents? ${post.hasParticipatedInAccidents ? 'Yes' : 'No'}</p>
+            <p>Is it high season? ${post.isHighSeason ? 'Yes' : 'No'}</p>
+            <h2>Rental price: $${rentalPrice.result}</h2>
+          </body>
+        </html>
+      `)
+    } else {
+      response.end(`
+        <html>
+          <body>
+            <h1> Error calculating rental price</h1>
+            <p>Something went wrong while trying to calculate the rental price. Please try again later.</p>
+            </body>
+            </html>
+            `)
+            }
+            }
+            })
+        
+        server.listen(3001)
+        
+        console.log('Server running at http://localhost:3001/')
