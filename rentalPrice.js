331c0050ca2b6235e5a881e593ea729cfc9ba8d0
hom4ok <55148431+hom4ok@users.noreply.github.com>
2023-03-03 10:37:40 +0200

    Update rentalPrice.js

--- rentalPrice.js
+++ rentalPrice.js
@@ -1,11 +1,13 @@
 // age - age of driver
-// licence - number of full years person holds driving licence
+// expirienceOfDriving - number of full years person has been driving
 // clazz - class of the car from 1 (smallest) to 5 (largest) that person wishes to rent
-// acc - has s/he caused any accidents within last year
-// acc2 - has s/he participated (but not caused) in any accidents within last year
+// hadAccidents - has s/he caused any accidents within last year
+// hadIncludedInAccidents - has s/he participated (but not caused) in any accidents within last year
 // season - if it is high season or not
-function price(age, licence, clazz, acc, acc2, season) {
+function price(age, expirienceOfDriving, clazz, hadAccidents, hadIncludedInAccidents, season) {
 
+    var rentalprice = age;
+    
     if (age < 18)
     {
       return "Driver too young - cannot quote the price";
@@ -14,20 +16,19 @@ function price(age, licence, clazz, acc, acc2, season) {
     {
         return "Drivers 21 y/o or less can only rent Class 1 vehicles";
     }
-    var rentalprice = age;
     if (clazz >= 4 && age <= 25 && season !== false)
     {
       rentalprice = rentalprice * 2;
     }
-    if (licence < 1)
+    if (expirienceOfDriving < 1)
     {
         return "Driver must hold driving licence at least for one year. Can not rent a car!";
     }
-    if (licence < 3)
+    if (expirienceOfDriving < 3)
     {
       rentalprice = rentalprice * 1.3;
     }
-    if (acc == true && age < 30)
+    if (hadAccidents == true && age < 30)
     {
       rentalprice = rentalprice + 15;
     }
