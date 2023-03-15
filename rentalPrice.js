eb7e123350147c375b52e8e09cc69533878f5b79
jordannassar <jordan.nassar@tptlive.ee>
2023-03-15 20:57:49 +0200

    update rentalPrice.js

--- rentalPrice.js
+++ rentalPrice.js
@@ -1,42 +1,61 @@
 // age - age of driver
-// licence - number of full years person holds driving licence
-// clazz - class of the car from 1 (smallest) to 5 (largest) that person wishes to rent
-// acc - has s/he caused any accidents within last year
-// acc2 - has s/he participated (but not caused) in any accidents within last year
-// season - if it is high season or not
-function price(age, license, clazz, acc, season) {
-	var rentalprice = age;
+// license - number of full years person holds driving license
+// carClass - class of the car from 1 (smallest) to 5 (largest) that person wishes to rent
+// causedAccidents - has s/he caused any accidents within last year
+// highSeason - if it is high season or not
+async function price(age, license, carClass, causedAccidents, highSeason) {
+	var rentalPrice = age;
+	var message;
 	if (age < 18) {
-		return 'Driver too young - cannot quote the price';
+		return { message: 'Driver too young - cannot quote the price' };
 	}
 
-	if (age < 30 && acc) {
-		rentalprice += 15;
+	if (age < 30 && causedAccidents) {
+		return {
+			rentalPrice: (rentalPrice += 15),
+		};
 	}
 
 	switch (license) {
 		case license < 1:
-			return 'Driver must hold driving license at least for one year. Can not rent a car!';
+			return {
+				message:
+					'Driver must hold driving license at least for one year. Can not rent a car!',
+			};
 		case license < 3 && license >= 1:
-			rentalprice *= 1.3;
-			return 'Since you have driving license for less than 3 years, you will be charged 30% more';
+			return {
+				rentalPrice: (rentalPrice *= 1.3),
+				message:
+					'Since you have driving license for less than 3 years, you will be charged 30% more',
+			};
 	}
 
-	switch (clazz) {
-		case clazz > 1 && age <= 21:
-			return 'Drivers 21 y/o or less can only rent Class 1 vehicles';
+	switch (carClass) {
+		case carClass > 1 && age <= 21:
+			return {
+				message:
+					'Drivers 21 y/o or less can only rent Class 1 vehicles',
+			};
 		case 4:
 		case 5:
-			if (age <= 25 && season) {
-				rentalprice *= 1.5;
-				return 'Drivers 25 y/o or less can only rent Class 1 vehicles';
+			if (age <= 25 && highSeason) {
+				return {
+					rentalPrice: (rentalPrice *= 1.5),
+					message:
+						'You will be charged 50% more for this car it being high season and your age being 25 or less',
+				};
 			}
 	}
 
-	if (rentalprice > 1000) {
-		return 1000.0;
+	if (rentalPrice > 1000) {
+		return {
+			rentalPrice: 1000.0,
+		};
 	}
 
-	return rentalprice;
+	return {
+		rentalPrice: rentalPrice,
+		message: message ? message : 'No special conditions apply',
+	};
 }
 exports.price = price;
