b4ef0c9ca16a78caefa6bc1532313007b4416ffb
Agnessa Tund <agnessa.tund@tptlive.ee>
2023-04-26 15:03:36 +0300

    refactored

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http = require('http')
 const qs = require('querystring')
-const rental = require('./rentalPrice')
+const {calculateRentalPrice} = require('./rentalPrice')
 
 const server = http.createServer(function(request, response) {
     console.dir(request.param)
@@ -15,46 +15,42 @@ const server = http.createServer(function(request, response) {
         request.on('end', function() {
             const post = qs.parse(body)
             console.log(post);
-            const result = rental.price(Number(post.age), Number(post.licence), Number(post.clazz), parseBool(post.acc), parseBool(post.acc2), parseBool(post.season))
+            const result = calculateRentalPrice
+            (Number(post.driverAge),
+            Number(post.driverLicenceYears), 
+            Number(post.carClass), 
+            post.recentAccidents === 'true',
+            post.isHighSeason === 'true'
+            );
             console.log(result);
             response.writeHead(200, {'Content-Type': 'text/html'})
             response.end('Result: ' + result)
         })
     } else {
-        var html = `
-            <html>
-                <body>
-                    <form id="calcForm" method="post" action="http://localhost:3000">Driver: <br>
-                        <input type="number" name="age"/>
-                        <label for="html">Your age?</label><br>
-                        <input type="number" name="licence" />
-                        <label for="html">How long have you had your licence?</label><br>
-                        <input type="number" name="clazz" />
-                        <label for="html">Class of the car (1 to 5)</label><br>
-                        <input type=checkbox name="acc" />
-                        <label for="html">Have you caused accidents?</label><br>
-                        <input type=checkbox name="acc2" />
-                        <label for="html">Have you participated in any accidents?</label><br>
-                        <input type=checkbox name="season" />
-                        <label for="html">Is it high season?</label><br>
-                        <input type="submit" value="Add" />
-                    </form>
-                </body>
-            </html>`
-        response.writeHead(200, {'Content-Type': 'text/html'})
-        response.end(html)
-    }
+        const html = `
+          <html>
+            <body>
+              <form id="calcForm" method="post" action="http://localhost:3000">Driver: <br>
+                <input type="number" name="driverAge"/>
+                <label for="html">Your age?</label><br>
+                <input type="number" name="driverLicenseYears" />
+                <label for="html">How long have you had your licence?</label><br>
+                <input type="number" name="carClass" />
+                <label for="html">Class of the car (1 to 5)</label><br>
+                <input type=checkbox name="recentAccidents" />
+                <label for="html">Have you caused accidents?</label><br>
+                <input type=checkbox name="isHighSeason" />
+                <label for="html">Is it high season?</label><br>
+                <input type="submit" value="Add" />
+              </form>
+            </body>
+          </html>`;
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.end(html);
+      }
+    });
 
-    function parseBool(val) {
-        let updatedVal = false;
-        if (val == 'on') {
-            updatedVal = true;
-        }
-        return updatedVal;
-    }
-})
-
-const port = 3000
-const host = '127.0.0.1'
-server.listen(port, host)
-console.log(`Listening at http://${host}:${port}`)
+const port = 3000;
+const host = '127.0.0.1';
+server.listen(port, host);
+console.log(`Server running at http://${host}:${port}/`);
--- rentalPrice.js
+++ rentalPrice.js
@@ -1,40 +1,36 @@
-// age - age of driver
-// licence - number of full years person holds driving licence
-// clazz - class of the car from 1 (smallest) to 5 (largest) that person wishes to rent
-// acc - has s/he caused any accidents within last year
-// acc2 - has s/he participated (but not caused) in any accidents within last year
-// season - if it is high season or not
-function price(age, licence, clazz, acc, acc2, season) {
+function calculateRentalPrice(driverAge, driverLicenseYears, carClass, recentAccidents, isHighSeason) 
+{
+  const MINIMUM_LICENSE_YEARS = 1;
+  const MINIMUM_DRIVING_AGE = 18;
+  const MAXIMUM_RENTAL_PRICE = 1000;
 
-    if (age < 18)
-    {
-      return "Driver too young - cannot quote the price";
-    }
-    if (age <= 21 && clazz > 2)
-    {
-        return "Drivers 21 y/o or less can only rent Class 1 vehicles";
-    }
-    var rentalprice = age;
-    if (clazz >= 4 && age <= 25 && season !== false)
-    {
-      rentalprice = rentalprice * 2;
-    }
-    if (licence < 1)
-    {
-        return "Driver must hold driving licence at least for one year. Can not rent a car!";
-    }
-    if (licence < 3)
-    {
-      rentalprice = rentalprice * 1.3;
-    }
-    if (acc == true && age < 30)
-    {
-      rentalprice = rentalprice + 15;
-    }
-    if (rentalprice > 1000)
-    {
-      return 1000.0;
-    }
-    return rentalprice;
+  let rentalPrice = driverAge;
+
+  if (driverAge < MINIMUM_DRIVING_AGE) {
+    return 'Driver is too young - cannot quote the price.';
+  }
+
+  if (driverAge <= 21 && carClass > 1) {
+    return 'Drivers 21 y/o or less can only rent Class 1 vehicles.';
+  }
+
+  if (driverLicenseYears < MINIMUM_LICENSE_YEARS) {
+    return 'Driver must hold driving license for at least one year. Cannot rent a car!';
+  }
+
+  if (driverLicenseYears < 3) {
+    rentalPrice *= 1.3;
+    return 'Since you have driving license for less than 3 years, you will be charged 30% more.';
+  }
+
+  if ((carClass === 4 || carClass === 5) && driverAge <= 25 && isHighSeason) {
+    rentalPrice *= 1.5;
+    return 'Drivers 25 years old or less renting a Class 4 or 5 vehicle during high season will be charged 50% more.';
+  }
+
+  if (recentAccidents && driverAge < 30) {
+    rentalPrice += 15;
+  }
+
+  return Math.min(rentalPrice, MAXIMUM_RENTAL_PRICE);
 }
-exports.price = price;
